af043a0c4b6a7d85fb61bbde305624a0a25c36f8
Dean Spooner <dean.spooner@me.com>
2021-05-15 18:20:17 +0100

    Initial setup complete.

---
+++ tests/test-setup.js
@@ -0,0 +1,4 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../.env.test' });
+//DEAN, THIS MAY BE WRONG AND ACTUALLY BE ONE DOT! Past project had as one dot, but directory doesn't make sense???
