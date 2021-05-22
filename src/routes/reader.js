c836e015da207f749f9d6ab978517d79d0488e1c
Dean Spooner <dean.spooner@me.com>
2021-05-22 17:44:00 +0100

    Passed all reader tests.

--- src/routes/reader.js
+++ src/routes/reader.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.post('/', readerController.create);
 
+router.get('/', readerController.findAll);
+
+router.get('/:id', readerController.findById);
+
+router.patch('/:id', readerController.update);
+
+router.delete('/:id', readerController.delete);
+
 module.exports = router;
