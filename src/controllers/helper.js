112f56edea8d6e769893361556ab16b88b33df5d
Dean Spooner <dean.spooner@me.com>
2021-05-25 21:14:29 +0100

    Removed password from responses.

--- src/controllers/helper.js
+++ src/controllers/helper.js
@@ -11,14 +11,24 @@ const getModel = (model) => {
 const readerError = { error: 'The reader could not be found.' }
 const bookError = { error: 'The book could not be found.' }
 
+const removePassword = (obj) => {
+    if (obj.hasOwnProperty('password')) {
+        delete obj.password;
+    }
+    return obj;
+};
+
 const create = async (model, req, res) => {
     const thisItem = await getModel(model).create(req.body)
-    res.status(201).send(thisItem);
+    res.status(201).send(removePassword(thisItem.dataValues));
 }
 
 const findAll = async (model, req, res) => {
     const allItems = await getModel(model).findAll();
-    res.status(200).send(allItems);
+    const itemsWithoutPassword = allItems.map((item) =>
+        removePassword(item.dataValues)
+    );
+    res.status(200).send(removePassword(itemsWithoutPassword));
 }
 
 const findById = async (model, req, res) => {
@@ -29,7 +39,7 @@ const findById = async (model, req, res) => {
     if (!thisItem && model == 'book') {
         return res.status(404).send(bookError);
     }
-    res.status(200).send(thisItem);
+    res.status(200).send(removePassword(thisItem.dataValues));
 }
 
 const update = async (model, req, res) => {
@@ -45,7 +55,7 @@ const update = async (model, req, res) => {
     });
 
     thisItem = await getModel(model).findByPk(req.params.id);
-    res.status(200).json(thisItem);
+    res.status(200).json(removePassword(thisItem.dataValues));
 };
 
 const remove = async (model, req, res) => {
@@ -59,7 +69,7 @@ const remove = async (model, req, res) => {
     await getModel(model).destroy({
         where: { id: req.params.id }
     });
-    res.status(204).json(thisItem);
+    res.status(204).json(removePassword(thisItem.dataValues));
 };
 
 module.exports = { create, findAll, findById, update, remove };
--- tests/reader.test.js
+++ tests/reader.test.js
@@ -116,7 +116,7 @@ describe('/readers', () => {
 
                     expect(reader.name).to.equal(expected.name);
                     expect(reader.email).to.equal(expected.email);
-                    expect(reader.password).to.equal(expected.password);
+                    expect(reader.password).to.equal(undefined);
                 });
             });
         });
@@ -129,7 +129,7 @@ describe('/readers', () => {
                 expect(response.status).to.equal(200);
                 expect(response.body.name).to.equal(reader.name);
                 expect(response.body.email).to.equal(reader.email);
-                expect(response.body.password).to.equal(reader.password);
+                expect(response.body.password).to.equal(undefined);
             });
 
             it('returns a 404 if the reader does not exist', async () => {
